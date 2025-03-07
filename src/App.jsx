c7ff2a4985759a4918f929c30336e355eeeaad64
RethikRaj <rethikrajrr@gmail.com>
2025-03-07 06:59:58 +0530

    configuring redux and creating userSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { RouterProvider } from "react-router-dom"
 import AppRouter from "./AppRouter"
+import {Provider} from "react-redux";
+import appStore from "./utils/appStore";
 
 const App = ()=>{
-  return <RouterProvider router={AppRouter}/>
+  return <Provider store={appStore}>
+    <RouterProvider router={AppRouter}/>
+  </Provider>
 }
 
 export default App

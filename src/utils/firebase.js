2fe544c369a38498ab49bfb1b5f2c0de56db954c
RethikRaj <rethikrajrr@gmail.com>
2025-09-10 06:41:49 +0530

    added security

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPlupXLucoIgFmG31LL9hom1O1soXtHDI",
-  authDomain: "zenflix-fc99d.firebaseapp.com",
-  projectId: "zenflix-fc99d",
-  storageBucket: "zenflix-fc99d.firebasestorage.app",
-  messagingSenderId: "905443246169",
-  appId: "1:905443246169:web:262d9e8b55cbb765abf148",
-  measurementId: "G-580K6Q9G47"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
